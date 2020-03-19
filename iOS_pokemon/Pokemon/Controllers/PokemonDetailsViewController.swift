9f65cc999b32fa49e9e3c9f09d943e2576246823
Ivann <ivann.galic@edu.itescia.fr>
2020-03-19 10:25:10 +0100

    avancée Image avec Claire

--- iOS_pokemon/Pokemon/Controllers/PokemonDetailsViewController.swift
+++ iOS_pokemon/Pokemon/Controllers/PokemonDetailsViewController.swift
@@ -4,15 +4,27 @@
 //
 //  Created by Claire on 24/03/2019.
 //  Copyright © 2019 Claire. All rights reserved.
-//
+
 
 import UIKit
 
 class PokemonDetailsViewController: UIViewController {
     var pokemon: Pokemon! // pokemon chosen from the list
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
+    @IBOutlet weak var photo: UIImageView!
+    override func viewDidLoad() {
+       super.viewDidLoad()
+        photo.image = nil
+        
+        if URL(string: pokemon.imageUrl) != nil {
+        // image du pokemon
+            let url = NSURL(string: pokemon.imageUrl)! as URL
+            if let data: NSData = NSData(contentsOf: url) {
+                photo.image = UIImage(data: data as Data)
+            } else {
+              photo.image = UIImage(named: "dragon")
+            }
     }
+}
 }
