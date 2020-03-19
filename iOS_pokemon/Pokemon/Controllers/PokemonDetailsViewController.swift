f3927670139a97604a639a3f5501a8c899077468
laurent <laurent.bodin58@gmail.com>
2020-03-19 13:11:24 +0100

    detailsViewController (en cours)

--- iOS_pokemon/Pokemon/Controllers/PokemonDetailsViewController.swift
+++ iOS_pokemon/Pokemon/Controllers/PokemonDetailsViewController.swift
@@ -12,10 +12,20 @@ class PokemonDetailsViewController: UIViewController {
     var pokemon: Pokemon! // pokemon chosen from the list
 
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var typeStackView: UIStackView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var pokemonNameLabel: UILabel!
     @IBOutlet weak var photo: UIImageView!
     override func viewDidLoad() {
        super.viewDidLoad()
         photo.image = nil
+        
+        // ad setup UI
+        pokemonNameLabel.text = pokemon.name
+        idLabel.text = "\(pokemon.pokedexId)"
+        descriptionLabel.text = pokemon.description
         
         if URL(string: pokemon.imageUrl) != nil {
         // image du pokemon
@@ -27,4 +37,5 @@ class PokemonDetailsViewController: UIViewController {
             }
     }
 }
+    private func setUpPokemonType([Type])
 }
--- iOS_pokemon/Pokemon/Models/Type.swift
+++ iOS_pokemon/Pokemon/Models/Type.swift
@@ -11,59 +11,59 @@ import UIKit
 
 enum Type: String {
     case bug
-    case dragon
-    case electric
-    case fairy
-    case fighting
-    case fire
-    case flying
-    case ghost
-    case grass
-    case ground
-    case ice
-    case normal
     case poison
-    case psychic
-    case rock
-    case steel
+    case electrik
+    case fire
     case water
-
-    func getIcon() -> UIImage? {
-        switch self {
+    case grass
+    case flying
+    case ground
+    case psychic
+    case ghost
+    case fighting
+    case dragon
+    case normal
+    case ice
+    case fairy
+    case steel
+    case rock
+    
+    func getIcon()->UIImage?{
+        switch self{
         case .bug:
-            return UIImage(named: "bug")
+            return UIImage(named :"bug")
+        case . poison:
+            return UIImage(named :"poison")
         case .dragon:
-            return UIImage(named: "dragon")
-        case .electric:
-            return UIImage(named: "electric")
+            return UIImage(named :"dragon")
+        case .electrik:
+            return UIImage(named :"electric")
         case .fairy:
-            return UIImage(named: "fairy")
+            return UIImage(named :"fairy")
         case .fighting:
-            return UIImage(named: "fighting")
+            return UIImage(named :"fighting")
         case .fire:
-            return UIImage(named: "fire")
+            return UIImage(named :"fire")
         case .flying:
-            return UIImage(named: "flying")
+            return UIImage(named :"flying")
         case .ghost:
-            return UIImage(named: "ghost")
-        case .grass:
-            return UIImage(named: "grass")
-        case .ground:
-            return UIImage(named: "ground")
-        case .ice:
-            return UIImage(named: "ice")
-        case .normal:
-            return UIImage(named: "normal")
-        case .poison:
-            return UIImage(named: "poison")
-        case .psychic:
-            return UIImage(named: "psychic")
-        case .rock:
-            return UIImage(named: "rock")
+            return UIImage(named :"ghost")
         case .steel:
-            return UIImage(named: "steel")
+            return UIImage(named :"steel")
+        case .normal:
+            return UIImage(named :"normal")
+        case .ice:
+            return UIImage(named :"ice")
+        case .ground:
+            return UIImage(named :"ground")
+        case .grass:
+            return UIImage(named :"grass")
         case .water:
-            return UIImage(named: "water")
+            return UIImage(named :"water")
+        case .psychic:
+            return UIImage(named :"psychic")
+        case .rock:
+            return UIImage(named :"rock")
         }
     }
 }
